d0e5fbe33710bbe84dc4b9484a17315ae0daa862
Abai Karagulov <60126610+Abai19@users.noreply.github.com>
2020-04-03 18:02:39 +0600

    Updates

---
+++ precache-manifest.5a924ccd27a3cce20a17cabbba408c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919f4d8eb7a82a7d428b9c4cab34f300",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "7ed4e38040a8c9d6112d",
+    "url": "/todo_react/static/css/main.d0d717ff.chunk.css"
+  },
+  {
+    "revision": "11fed2d414e5de5ee9ae",
+    "url": "/todo_react/static/js/2.4304b90b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_react/static/js/2.4304b90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed4e38040a8c9d6112d",
+    "url": "/todo_react/static/js/main.2e221ee9.chunk.js"
+  },
+  {
+    "revision": "6b0d414212b7a6973878",
+    "url": "/todo_react/static/js/runtime-main.bdc457bc.js"
+  }
+]);
